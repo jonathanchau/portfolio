a2d9bb70d09246e3edd53313328b89221ee8a684
JonathanChau145 <jonathan.r.chau@gmail.com>
2026-04-17 16:09:57 -0700

    fixed 404 error

--- global.js
+++ global.js
@@ -15,7 +15,7 @@ const pages = [
 const BASE_PATH =
   location.hostname === 'localhost' || location.hostname === '127.0.0.1'
     ? '/'
-    : '/';
+    : '/portfolio/';
 
 let nav = document.createElement('nav');
 document.body.prepend(nav);
